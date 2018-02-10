eaadf78711b7140947e3da545e36f65952ff8c32
Haotian Wang <haotian.wang@duke.edu>
2018-02-10 02:56:51 -0500

    rewrote findCoords.py so that it used sqlite3 to cache results

--- findCoords.py
+++ findCoords.py
@@ -1,17 +1,40 @@
 from geopy.geocoders import GoogleV3
+import sqlite3
 from time import sleep
 
+class Cache:
+    def __init__(self, fn='cache.db'):
+       self.conn = conn = sqlite3.connect(fn)
+       cur = conn.cursor()
+       cur.execute('CREATE TABLE IF NOT EXISTS '
+                   'Geo ( '
+                   'address STRING PRIMARY KEY, '
+                   'latitude DECIMAL(20,17),'
+                   'longitude DECIMAL(20,17)'
+                   ')')
+       conn.commit()
+
+    def address_cached(self, address):
+        cur = self.conn.cursor()
+        cur.execute('SELECT latitude FROM Geo WHERE address=?', (address,))
+        res = cur.fetchone()
+        if res is None: return False
+
+    def save_to_cache(self, address, latitude, longitude):
+        cur = self.conn.cursor()
+        cur.execute('INSERT INTO Geo(address, latitude, longitude) VALUES(?, ?, ?)',
+                    (address, latitude, longitude))
+
+
 geolocator = GoogleV3("AIzaSyDDwvAnPVqH0cK_-9cYJDxUsanFgjXJduw")
-with open("coordinates.txt",'w') as f:
-    with open("stationNames.txt",'r') as source:
-        for line in source:
+with open("stationNames.txt",'r') as source:
+    for line in source:
+        cache = Cache()
+        location = cache.address_cached(line)
+        if location:
+            continue
+        else:
             location = geolocator.geocode(line)
-            if location:
-                lat = location.latitude
-                lon = location.longitude
-            else:
-                lat = None
-                lon = None
-            coords = "{}, {}".format(lat,lon)
-            f.write(coords+'\n')
-            sleep(0.3)
+            cache.save_to_cache(line, location.latitude, location.longitude)
+        sleep(0.3)
+
